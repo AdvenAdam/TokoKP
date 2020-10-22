e78e711178f4e16702863088fe79ec264ed2b0cf
AdvenAdam <AdvenAdam@github.com>
2020-10-22 12:34:43 +0700

    a

--- app/Views/layout/v_sidebar.php
+++ app/Views/layout/v_sidebar.php
@@ -19,23 +19,6 @@
                 </a>
             </li>
             <li>
-                <a href="#ui-elements" data-toggle="collapse" aria-expanded="false">
-                    <span class="link-title">UI Elements</span>
-                    <i class="mdi mdi-bullseye link-icon"></i>
-                </a>
-                <ul class="collapse navigation-submenu" id="ui-elements">
-                    <li>
-                        <a href="pages/ui-components/buttons.html">Buttons</a>
-                    </li>
-                    <li>
-                        <a href="pages/ui-components/tables.html">Tables</a>
-                    </li>
-                    <li>
-                        <a href="pages/ui-components/typography.html">Typography</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
                 <a href="#komponen" data-toggle="collapse" aria-expanded="false">
                     <span class="link-title">CRUD Komponen</span>
                     <i class="mdi mdi-library-plus link-icon"></i>
@@ -73,23 +56,12 @@
 
             <li>
                 <a href="pages/forms/form-elements.html">
-                    <span class="link-title">Forms</span>
+                    <span class="link-title">Link</span>
                     <i class="mdi mdi-clipboard-outline link-icon"></i>
                 </a>
             </li>
-            <li>
-                <a href="psu">
-                    <span class="link-title">Charts</span>
-                    <i class="mdi mdi-chart-donut link-icon"></i>
-                </a>
-            </li>
-            <li>
-                <a href="pages/icons/material-icons.html">
-                    <span class="link-title">Icons</span>
-                    <i class="mdi mdi-flower-tulip-outline link-icon"></i>
-                </a>
-            </li>
-            <li class="nav-category-divider">DOCS</li>
+
+            <li class="nav-category-divider">Akun</li>
             <li>
                 <a href="/zz/src/docs/docs.html">
                     <span class="link-title">Documentation</span>
